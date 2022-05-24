de781c857cb776301bf3c069f285b5d6a439e4ea
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-05-24 22:43:37 +0200

    HW 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
+'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const setBgColor = document.querySelector("body");
+const onBtnChangeColor = document.querySelector(".change-color");
+const numbChangeColor = document.querySelector(".color")
+
+const changeColor = (event => {
+  event.preventDefault();
+
+  setBgColor.style.backgroundColor = getRandomHexColor();
+  numbChangeColor.textContent = getRandomHexColor();
+});
+
+onBtnChangeColor.addEventListener("click", changeColor);

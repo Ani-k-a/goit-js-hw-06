416126d1d6500dd43dd14765596130c17dddf8f0
Hanna Lytvynenko <ani-k-a@ukr.net>
2022-05-30 21:34:37 +0200

    update task 5

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,7 @@ const formTitleEl = document.querySelector("#name-output");
 
 formInputEl.addEventListener('input', event => {
     formTitleEl.textContent = event.target.value;
+    
     if (event.target.value === "") {
         formTitleEl.textContent = "Anonymous";
     }
